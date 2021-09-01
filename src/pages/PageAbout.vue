45dd3be264b3d0ab227b4d39399845fef115ec4f
Tyson Romero <romennoodles23@yahoo.com>
2021-09-01 13:00:30 -0600

    started over to do it again-first commit

---
+++ src/pages/PageAbout.vue
@@ -0,0 +1,23 @@
+<template>
+  <q-page class="q-pa-lg">
+    <h4 class="q-mt-none q-mb-md text-weight-bold">About Qwitter</h4>
+    <div class="text-body1">
+      <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+      Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+      when an unknown printer took a galley of type and scrambled it to make a type 
+      specimen book.</p>
+      <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+      Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+      when an unknown printer took a galley of type and scrambled it to make a type 
+      specimen book.</p>
+    </div>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'PageAbout'
+})
+</script>
---
+++ src/pages/PageHome.vue
@@ -0,0 +1,58 @@
+<template>
+  <q-page>
+    <div class="q-py-lg q-px-md row items-end q-col-gutter-md">
+      <div class="col">
+        <q-input
+          placeholder="What's Happening?"
+          bottom-slots
+          v-model="newQweetContent"
+          maxlength="280"
+          counter 
+          autogrow
+          class="new-qweet"
+        >
+        <template v-slot:before>
+          <q-avatar size="xl">
+            <img src="https://s.gravatar.com/avatar/a3020caca3fca25b245e374a01505dc4?s=80">
+          </q-avatar>
+        </template>
+
+        <template v-slot:after>
+         
+        </template>
+      </q-input>
+      </div>
+      <div class="col col-shrink">
+         <q-btn
+            :disable="!newQweetContent"
+            unelevated
+            rounded
+            color="primary"
+            label="Qweet" 
+            no-caps
+            class="q-mb-lg"
+          />
+      </div>
+    </div>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'PageHome',
+  data() {
+    return {
+      newQweetContent: ''
+    }
+  }
+})
+</script>
+
+<style lang="sass">
+.new-qweet
+  textarea
+    font-size: 19px
+    line-height: 1.4 !important
+</style>
